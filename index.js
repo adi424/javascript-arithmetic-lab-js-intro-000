8614ec81236362cf6da354212a324880a9a95b36
adi dabush <adi.dabush@wework.com>
2019-07-25 15:09:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b){
+  a + b;
+  var result = a + b;
+  return result
+}
+
+function subtract(a,b){
+  a - b;
+  var result = a - b;
+  return result
+}
+
+function multiply(a,b){
+  a * b;
+  var result = a * b;
+  return result
+}
+
+function divide(a,b){
+  a / b;
+  var result = a / b;
+  return result
+}
+
+function inc(n){
+  n++
+  var result = n++
+  return result
+}
+
+function dec(n){
+  n--
+  var result = n--
+  return result
+}
+
+function makeInt(n){
+parseInt('n', 10)
+  return n
+}
